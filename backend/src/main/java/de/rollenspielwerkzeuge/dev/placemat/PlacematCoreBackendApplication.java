e56e8fc2a9782c1e947e098e3104f105052e3c92
Sacha Dominguez Rodriguez <sacha.dr.dev@gmail.com>
2026-01-11 09:09:10 +0100

    Added previously uncommitted files.

---
+++ backend/src/main/java/de/rollenspielwerkzeuge/dev/placemat/PlacematCoreBackendApplication.java
@@ -0,0 +1,13 @@
+package de.rollenspielwerkzeuge.dev.placemat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacematCoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacematCoreBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/rollenspielwerkzeuge/dev/placemat/PlacematCoreBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package de.rollenspielwerkzeuge.dev.placemat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class PlacematCoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
